6b64f421c42a0ac2a398116edfecfc6d50b45e85
Max Kotlan <max.maxkot@gmail.com>
2016-11-13 10:21:07 -0500

    fixed uv's for grass

--- Assets/Chunk.cs
+++ Assets/Chunk.cs
@@ -41,25 +41,24 @@ public class Chunk : MonoBehaviour {
             {
                 for (int z = 0; z < width; z++)
                 {
-                    if (y < (height - 3))
+                    if (y < (height - 1))
                     {
-                        if (Mathf.RoundToInt(Random.Range(0,51)) != 25)
-                        {
-                            map[x, y, z] = new block(new Vector3(x, y, z), 3);
-                        }
-                        else
+                        if (Mathf.RoundToInt(Random.Range(0,51)) == 25)
                         {
                             map[x, y, z] = new block(new Vector3(x, y, z), 5);
                         }
-                    } else
-                    {
-                        if (y < (height - 1))
+                        else if (Mathf.RoundToInt(Random.Range(0, 2)) == 1)
                         {
                             map[x, y, z] = new block(new Vector3(x, y, z), 2);
                         } else
                         {
-                            map[x, y, z] = new block(new Vector3(x, y, z), 1);
+                            map[x, y, z] = new block(new Vector3(x, y, z), 3);
                         }
+                    } else
+                    {
+          
+                      map[x, y, z] = new block(new Vector3(x, y, z), 1);
+                 
                     }
                 }
             }
@@ -85,43 +84,43 @@ public class Chunk : MonoBehaviour {
         {
             offset1 = Vector3.left;
             offset2 = Vector3.back;
-            DrawFace(start + Vector3.right, offset1, offset2, block);
+            DrawFace(start + Vector3.right, offset1, offset2, block, Vector3.down);
         }
         if (isTransparent(x, y + 1, z))
         {
             offset1 = Vector3.right;
             offset2 = Vector3.back;
-            DrawFace(start + Vector3.up, offset1, offset2, block);
+            DrawFace(start + Vector3.up, offset1, offset2, block, Vector3.up);
         }
 
         if (isTransparent(x - 1, y, z))
         {
             offset1 = Vector3.up;
             offset2 = Vector3.back;
-            DrawFace(start, offset1, offset2, block);
+            DrawFace(start, offset1, offset2, block, Vector3.left);
         }
         if (isTransparent(x + 1, y, z))
         {
             offset1 = Vector3.down;
             offset2 = Vector3.back;
-            DrawFace(start + Vector3.right + Vector3.up, offset1, offset2, block);
+            DrawFace(start + Vector3.right + Vector3.up, offset1, offset2, block, Vector3.right);
         }
 
         if (isTransparent(x, y, z - 1))
         {
             offset1 = Vector3.left;
             offset2 = Vector3.up;
-            DrawFace(start + Vector3.right + Vector3.back, offset1, offset2, block);
+            DrawFace(start + Vector3.right + Vector3.back, offset1, offset2, block, Vector3.back);
         }
         if (isTransparent(x, y, z + 1))
         {
             offset1 = Vector3.right;
             offset2 = Vector3.up;
-            DrawFace(start, offset1, offset2, block);
+            DrawFace(start, offset1, offset2, block, Vector3.forward);
         }
     }
 
-    public void DrawFace(Vector3 start, Vector3 offset1, Vector3 offset2, block block)
+    public void DrawFace(Vector3 start, Vector3 offset1, Vector3 offset2, block block, Vector3 direction)
     {
         int index = verts.Count;
 
@@ -133,7 +132,13 @@ public class Chunk : MonoBehaviour {
         int textureid = Mathf.RoundToInt(Random.Range(1, 7));
         Vector2 uvBase = new Vector2(0.0f, .75f);
 
-        switch (block.materialID)
+        int matid = block.materialID;
+        if (matid == 1 && direction == Vector3.up)
+        {
+            matid = 8;
+        }
+
+        switch (matid)
         {
             case 1:
                 uvBase = new Vector2(0.0f, .75f);
@@ -156,12 +161,30 @@ public class Chunk : MonoBehaviour {
             case 7:
                 uvBase = new Vector2(.5f, .5f);
                 break;
+            case 8:
+                uvBase = new Vector2(.75f, .5f);
+                break;
         }
 
-        uv.Add(uvBase);
-        uv.Add(uvBase + new Vector2(.25f, 0));
-        uv.Add(uvBase + new Vector2(0, .25f));
-        uv.Add(uvBase + new Vector2(.25f, .25f));
+        if (direction == Vector3.right)
+        {
+            uv.Add(uvBase + new Vector2(.25f, .25f));
+            uv.Add(uvBase + new Vector2(.25f, 0));
+            uv.Add(uvBase + new Vector2(0, .25f));
+            uv.Add(uvBase);
+        } else if (direction == Vector3.left)
+        {
+            uv.Add(uvBase);
+            uv.Add(uvBase + new Vector2(0, .25f));
+            uv.Add(uvBase + new Vector2(.25f, 0));
+            uv.Add(uvBase + new Vector2(.25f, .25f));
+        } else 
+        { 
+            uv.Add(uvBase);
+            uv.Add(uvBase + new Vector2(.25f, 0));
+            uv.Add(uvBase + new Vector2(0, .25f));
+            uv.Add(uvBase + new Vector2(.25f, .25f));
+        }
 
 
         tris.Add(index + 0);
